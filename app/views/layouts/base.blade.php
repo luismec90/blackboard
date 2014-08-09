15e1ce472901768aaa8cc1424713c86857dba09e
Luis Fernando Montoya <luismec90@gmail.com>
2014-08-09 16:12:47 -0500

    Mejoras

---
+++ app/views/layouts/base.blade.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            @section('title')
+            Titulo
+            @show
+        </title>
+        @section('css')
+
+        @show
+
+    </head>
+    <body>
+        @yield('content')
+
+        @section('js')
+
+        @show
+    </body>
+</html>
