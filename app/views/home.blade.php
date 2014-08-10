577013983c1ee79e273e484076f672115d766c84
Luis Fernando Montoya <luismec90@gmail.com>
2014-08-10 09:10:47 -0500

    Mejoras

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,17 +1,2 @@
 @extends('layouts/base')
 
-@section('title')
-    Titulo2
-@stop
-
-@section('css')
-   css
-@stop
-
-@section('content')
-    <p>Contenido</p>
-@stop
-
-@section('js')
-  js
-@stop
